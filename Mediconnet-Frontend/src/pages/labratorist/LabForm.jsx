440016870cbe60e5d6e570b5133c7570c08c8839
Firaol Ayana <loarifse16@gmail.com>
2025-05-22 10:54:39 +0300

    Update LabForm.jsx

--- Mediconnet-Frontend/src/pages/labratorist/LabForm.jsx
+++ Mediconnet-Frontend/src/pages/labratorist/LabForm.jsx
@@ -17,7 +17,7 @@ import {
 } from '@mui/material';
 import axios from 'axios';
 import { format } from 'date-fns';
-const BASE_URL = "http://localhost:5500";
+const BASE_URL = "https://mediconnet-backend.onrender.com";
 
 const LabForm = () => {
   const { id } = useParams();
